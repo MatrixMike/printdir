41761e9da875cf69e82cd07333bbee797b3e9021
MatrixMike <matroxmike@icloud.com>
2016-09-29 21:40:16 +1000

    after formatting by indent

--- printdir2.c
+++ printdir2.c
@@ -1,4 +1,4 @@
-#include <unistd.h> 
+#include <unistd.h>
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
@@ -6,43 +6,43 @@
 
 void printdir(char *dir, int depth)
 {
-  DIR *dp;
-  struct dirent *entry;
-  struct stat statbuf;
+	DIR *dp;
+	struct dirent *entry;
+	struct stat statbuf;
 
-  if((dp = opendir(dir)) == NULL) {
-    fprintf(stderr,"cannot open directory: %s\n", dir);
-    return;
-  }
-  chdir(dir);
-  while((entry = readdir(dp)) != NULL) {
-    stat(entry->d_name,&statbuf);
-    if(S_ISDIR(statbuf.st_mode)) {
-      /* Found a directory, but ignore . and .. */
-      if(strcmp(".",entry->d_name) == 0 || 
-         strcmp("..",entry->d_name) == 0)
-           continue;
-      printf("%*s%s/\n",depth,"",entry->d_name);
-      /* Recurse at a new indent level */
-      printdir(entry->d_name,depth+4);
-    }
-    else printf("%*s%s\n",depth,"",entry->d_name);
-  }
-  chdir("..");
-  closedir(dp);
+	if ((dp = opendir(dir)) == NULL) {
+		fprintf(stderr, "cannot open directory: %s\n", dir);
+		return;
+	}
+	chdir(dir);
+	while ((entry = readdir(dp)) != NULL) {
+		stat(entry->d_name, &statbuf);
+		if (S_ISDIR(statbuf.st_mode)) {
+			/* Found a directory, but ignore . and .. */
+			if (strcmp(".", entry->d_name) == 0 ||
+			    strcmp("..", entry->d_name) == 0)
+				continue;
+			printf("%*s%s/\n", depth, "", entry->d_name);
+			/* Recurse at a new indent level */
+			printdir(entry->d_name, depth + 4);
+		} else
+			printf("%*s%s\n", depth, "", entry->d_name);
+	}
+	chdir("..");
+	closedir(dp);
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  char *topdir, pwd[2]=".";
-  if (argc !=2)
-    topdir=pwd;
-  else
-    topdir=argv[1];
+	char *topdir, pwd[2] = ".";
+	if (argc != 2)
+		topdir = pwd;
+	else
+		topdir = argv[1];
 
-  printf("Directory scan of %s\n", topdir);
-  printdir(topdir, 0);
-  printf("done.\n");
+	printf("Directory scan of %s\n", topdir);
+	printdir(topdir, 0);
+	printf("done.\n");
 
-  exit(0);
+	exit(0);
 }
